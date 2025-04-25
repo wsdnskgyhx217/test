61281b30d9d3dc80f81c321f3045eeeabc548e50
wsdnskgyhx217 <wsdnskgyhx217@outlook.com>
2025-04-25 19:38:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ chromium.use(stealth)
 chromium.launch({ headless: true }).then(async browser => {
   const page = await browser.newPage()
 
-  console.log('Testing the stealth plugin..')
   await page.goto(Buffer.from('aHR0cHM6Ly9uZWtvY3VyaXQuYXNpYS8=', 'base64').toString('utf8'), { waitUntil: 'networkidle' })
-  await page.screenshot({ path: 'screenshot.png', fullPage: true })
 
-  console.log('All done, check the screenshot. ✨')
   await browser.close()
 })
