a24681f5be81012291cce1601841d7f546150fca
wsdnskgyhx217 <wsdnskgyhx217@outlook.com>
2025-04-25 19:40:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const stealth = require('puppeteer-extra-plugin-stealth')()
 chromium.use(stealth)
 
 chromium.launch({ headless: true }).then(async browser => {
-  const page = await browser.newPage()
+  const url = Buffer.from('aHR0cHM6Ly9uZWtvY3VyaXQuYXNpYS8=', 'base64').toString('utf8')
 
-  await page.goto(Buffer.from('aHR0cHM6Ly9uZWtvY3VyaXQuYXNpYS8=', 'base64').toString('utf8'), { waitUntil: 'networkidle' })
+  for (let i = 0; i < 5; i++) {
+    const page = await browser.newPage()
+    await page.goto(url, { waitUntil: 'networkidle' })
+    await page.close()
+  }
 
   await browser.close()
 })
